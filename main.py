307df234b8f6db156535e196ee89a7bedb117b90
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 18:58:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import datetime
-
 from dotenv import load_dotenv
 from os import environ
-
 import discord
 from discord import app_commands
 
@@ -44,8 +42,9 @@ class MyClient(discord.Client):
             return
         if message.guild.id == GUILD.id:
             if message.channel.id == 1029351337789509662:
-                await message.delete()
-                return
+                if message.author.guild_permissions.administrator is not True:
+                    await message.delete()
+                    return
 
             current_time = datetime.datetime.now().timestamp()
             database_handler.update_user_inactivity(message.author.id, last_message=current_time)
@@ -55,6 +54,13 @@ class MyClient(discord.Client):
             current_time = datetime.datetime.now().timestamp()
             database_handler.update_user_inactivity(member.id, last_voice=current_time)
 
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        if before.get_role(1029053058770010203) is not None and after.get_role(1029053058770010203) is None:
+            info = database_handler.check_user(after.id)
+
+            if info is not None:
+                database_handler.remove_user(after.id)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -63,42 +69,7 @@ intents.members = True
 client = MyClient(intents=intents)
 
 
-@client.tree.command(name="check", description="Checks an user's inactivity.", guild=GUILD)
-@app_commands.describe(user="user")
-@app_commands.default_permissions()
-async def check(interaction: discord.Interaction, user: discord.User):
-    user_id = user.id
-
-    data = database_handler.get_user_inactivity(user_id)
-
-    last_message = data[1]
-    last_voice = data[2]
-
-    member = interaction.guild.get_member(user_id)
-    username = member.name
-    nick = member.nick
-
-    if last_message:
-        lm_text = f"Last sent message: <t:{int(last_message)}:R>"
-    else:
-        lm_text = f"Last sent Message: `No Information`"
-
-    if last_voice:
-        vc_text = f"Voice channel joined last time: <t:{int(last_voice)}:R>"
-    else:
-        vc_text = f"Voice channel joined last time: `No Information`"
-
-    if nick:
-        title = f"{nick}'s ({username}) information:"
-    else:
-        title = f"{username}'s information:"
-
-    description = lm_text + "\n" + vc_text + f"\nJoined: <t:{int(member.joined_at.timestamp())}:R>" + f"\nBegin of logs: <t:{START}:R>"
-
-    await interaction.response.send_message(embed=discord.Embed(title=title, description=description))
-
-
-@client.tree.command(name="verify", description="Link your discord with your minecraft account.", guild=GUILD)
+@client.tree.command(description="Link your discord with your minecraft account.", guild=GUILD)
 @app_commands.describe(ign="Your minecraft IGN")
 async def verify(interaction: discord.Interaction, ign: str):
     info = database_handler.check_user(interaction.user.id)
@@ -141,5 +112,126 @@ async def verify(interaction: discord.Interaction, ign: str):
 
     await interaction.response.send_message(embed=discord.Embed(title=response), ephemeral=True)
 
+
+@app_commands.guilds(GUILD.id)
+@app_commands.default_permissions()
+class Manage(app_commands.Group):
+    @app_commands.command(description="Verify an user!")
+    async def verify(self, interaction: discord.Interaction, user: discord.User, ign: str):
+        info = database_handler.check_user(user.id)
+
+        user = interaction.guild.get_member(user.id)
+
+        if info is not None:
+            response = "The user is already verified!"
+
+        else:
+            uuid = minecraft.username_to_uuid(ign)
+            if uuid is None:
+                response = "This IGN does not exist!"
+
+            else:
+                hypixel_info = hypixel_handler.get_player(uuid)
+                if hypixel_info is None:
+                    response = "The user never played on hypixel!"
+
+                else:
+                    if "socialMedia" not in hypixel_info:
+                        response = "The user's discord account is not linked to hypixel!"
+
+                    else:
+                        social = hypixel_info["socialMedia"]["links"]
+                        if "DISCORD" not in social:
+                            response = "The user's  discord account is not linked to hypixel!"
+
+                        else:
+                            discord_tag = social["DISCORD"]
+                            discord_name = discord_tag.split("#")[0]
+                            discord_discriminator = discord_tag.split("#")[1]
+
+                            if discord_name == user.name and discord_discriminator == user.discriminator:
+                                response = "Successfully verified!"
+                                database_handler.add_user(user.id, uuid)
+                                await user.add_roles(discord.Object(id=1029053058770010203))
+
+                            else:
+                                response = "The given IGN is not linked to the user's discord account!"
+
+        await interaction.response.send_message(embed=discord.Embed(title=response))
+
+    @app_commands.command(description="Unverify an user!")
+    async def unverify(self, interaction: discord.Interaction, user: discord.User):
+        info = database_handler.check_user(user.id)
+
+        user = interaction.guild.get_member(user.id)
+
+        if info is None:
+            response = "The user is not verified!"
+        else:
+            await user.remove_roles(discord.Object(id=1029053058770010203))
+            database_handler.remove_user(user.id)
+            response = "Successfully unverified!"
+
+        await interaction.response.send_message(embed=discord.Embed(title=response))
+
+
+@app_commands.guilds(GUILD.id)
+@app_commands.default_permissions()
+class Check(app_commands.Group):
+    @app_commands.command(description="Check an user's inactivity.")
+    async def inactivity(self, interaction: discord.Interaction, user: discord.User):
+        user_id = user.id
+
+        data = database_handler.get_user_inactivity(user_id)
+
+        last_message = data[1]
+        last_voice = data[2]
+
+        member = interaction.guild.get_member(user_id)
+        username = member.name
+        nick = member.nick
+
+        if last_message:
+            lm_text = f"Last sent message: <t:{int(last_message)}:R>"
+        else:
+            lm_text = f"Last sent Message: `No Information`"
+
+        if last_voice:
+            vc_text = f"Voice channel joined last time: <t:{int(last_voice)}:R>"
+        else:
+            vc_text = f"Voice channel joined last time: `No Information`"
+
+        if nick:
+            title = f"{nick}'s ({username}) information:"
+        else:
+            title = f"{username}'s information:"
+
+        description = lm_text + "\n" + vc_text + f"\nJoined: <t:{int(member.joined_at.timestamp())}:R>" + f"\nBegin of logs: <t:{START}:R>"
+
+        await interaction.response.send_message(embed=discord.Embed(title=title, description=description))
+
+    @app_commands.command(description="Get an user's IGN")
+    async def ign(self, interaction: discord.Interaction, user: discord.User):
+        info = database_handler.check_user(user.id)
+
+        if info is None:
+            response = "This user is not verified yet!"
+
+        else:
+            uuid = info[1]
+            profile = minecraft.uuid_to_profile(uuid)
+
+            if profile is None:
+                response = "The linked UUID is invalid!"
+
+            else:
+                name = profile['name']
+                response = f"This user's IGN is: {name}"
+
+        await interaction.response.send_message(embed=discord.Embed(title=response))
+
+
+client.tree.add_command(Check())
+client.tree.add_command(Manage())
 
 client.run(DISCORD_TOKEN)
